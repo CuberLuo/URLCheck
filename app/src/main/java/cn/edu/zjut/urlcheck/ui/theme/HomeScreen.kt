d3f761529c576ade9b414b437255457edceb549e
CuberLuo <1501858187@qq.com>
2023-03-08 20:36:38 +0800

    新增相机权限检查,防止未授权进行activity跳转

--- app/src/main/java/cn/edu/zjut/urlcheck/ui/theme/HomeScreen.kt
+++ app/src/main/java/cn/edu/zjut/urlcheck/ui/theme/HomeScreen.kt
@@ -1,6 +1,10 @@
 package cn.edu.zjut.urlcheck.ui.theme
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
@@ -22,7 +26,9 @@ import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import cn.edu.zjut.urlcheck.activities.QrCodeScanActivity
+import cn.edu.zjut.urlcheck.utils.DialogUtil
 import cn.edu.zjut.urlcheck.utils.LogUtil
 import cn.edu.zjut.urlcheck.utils.RequestUtil
 import cn.edu.zjut.urlcheck.utils.UrlJudgeUtil
@@ -33,71 +39,101 @@ import retrofit2.Response
 
 
 @Composable
-fun HomeScreen(){
+fun HomeScreen() {
 
-        Box(modifier = Modifier
-            .fillMaxSize())
-        {
-            Column {
-                Spacer(modifier = Modifier.size(70.dp))
-                ScanQrCode()
-                SearchText()
-
-            }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    )
+    {
+        Column {
+            Spacer(modifier = Modifier.size(70.dp))
+            ScanQrCode()
+            SearchText()
         }
+    }
 }
+
 var resultsText by mutableStateOf("There is no result")
+
 @Composable
-fun ScanQrCode(){
-    Box(Modifier
-        .fillMaxWidth(),
+fun ScanQrCode() {
+    Box(
+        Modifier.fillMaxWidth(),
         contentAlignment = Alignment.Center
-    ){
+    ) {
         Column(
             modifier = Modifier.padding(15.dp),
             verticalArrangement = Arrangement.Center
-        ){
-            val target = Intent(LocalContext.current, QrCodeScanActivity::class.java)
-            val launcher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()){ activityResult ->
-                activityResult.data?.apply {
-                    val result = getStringExtra("SCAN_RESULT").toString()
-                    //resultsText=result
-                    LogUtil.logInfo(result)
-                    if(!UrlJudgeUtil().getCompleteUrl(result)){
-                        resultsText="扫码结果:$result\n\n该二维码不包含URL链接"
-                    }else{
-                        val call: Call<ResponseBody> = RequestUtil.service.getQRCode(result)
-                        call.enqueue(
-                            object : Callback<ResponseBody> {
-                                override fun onResponse(
-                                    call: Call<ResponseBody>,
-                                    response: Response<ResponseBody>
-                                ) {
-                                    val s: String = response.body()!!.string()
-                                    resultsText="扫码结果$result\n\n$s"
-                                }
-
-                                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                                    resultsText=t.toString()
-
-                                }
-                            }
-                        )
-                    }
+        ) {
+            val context = LocalContext.current
+            val target = Intent(context, QrCodeScanActivity::class.java)
+            val permissionLauncher = rememberLauncherForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    LogUtil.logInfo("PERMISSION GRANTED")
+                } else {
+                    Toast.makeText(context,"相机权限未开启!",Toast.LENGTH_SHORT).show()
+                    LogUtil.logInfo("PERMISSION DENIED")
                 }
             }
+            val launcher =
+                rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+                    activityResult.data?.apply {
+                        val result = getStringExtra("SCAN_RESULT").toString()
+                        //resultsText=result
+                        LogUtil.logInfo(result)
+                        if (!UrlJudgeUtil().getCompleteUrl(result)) {
+                            resultsText = "扫码结果:$result\n\n该二维码不包含URL链接"
+                        } else {
+                            val call: Call<ResponseBody> = RequestUtil.service.getQRCode(result)
+                            call.enqueue(
+                                object : Callback<ResponseBody> {
+                                    override fun onResponse(
+                                        call: Call<ResponseBody>,
+                                        response: Response<ResponseBody>
+                                    ) {
+                                        val s: String = response.body()!!.string()
+                                        resultsText = "扫码结果$result\n\n$s"
+                                    }
+
+                                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                                        resultsText = t.toString()
+
+                                    }
+                                }
+                            )
+                        }
+                    }
+                }
             Button(
                 onClick = {
-                    launcher.launch(target)
+                    when (PackageManager.PERMISSION_GRANTED){
+                        ContextCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.CAMERA
+                        ) -> {
+                            LogUtil.logInfo("Camera permission has been granted")
+                            launcher.launch(target)
+                        }
+                        else -> {
+                            permissionLauncher.launch(Manifest.permission.CAMERA)
+                        }
+                    }
+
                 },
-                modifier = Modifier.size(130.dp,130.dp),
-                colors = ButtonDefaults.buttonColors(backgroundColor = CardBlue)) {
+                modifier = Modifier.size(130.dp, 130.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = CardBlue)
+            ) {
                 Text(text = "scan", color = Color.White)
             }
-            Text(text = "二维码检测",
+            Text(
+                text = "二维码检测",
                 color = QrTextColor,
-                modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
-                    .padding(0.dp,10.dp)
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterHorizontally)
+                    .padding(0.dp, 10.dp)
             )
 
         }
@@ -108,25 +144,28 @@ fun ScanQrCode(){
 }
 
 @Composable
-fun SearchText(){
+fun SearchText() {
 
     var text by remember {
-        mutableStateOf("") }
+        mutableStateOf("")
+    }
     var isURL by remember {
-        mutableStateOf(true) }
+        mutableStateOf(true)
+    }
 
     val focusManager = LocalFocusManager.current
 
-    Box(Modifier
-        .fillMaxWidth(),
+    Box(
+        Modifier
+            .fillMaxWidth(),
         contentAlignment = Alignment.Center
     ) {
         Column(
-            modifier = Modifier.padding(15.dp,0.dp),
+            modifier = Modifier.padding(15.dp, 0.dp),
             verticalArrangement = Arrangement.Center
         ) {
             BasicTextField(
-                maxLines=2,
+                maxLines = 2,
                 value = text,
                 onValueChange = {
                     text = it
@@ -139,35 +178,38 @@ fun SearchText(){
                     .background(BabyBlue, RoundedCornerShape(percent = 30))
                     .padding(5.dp)
                     .background(BabyBlue, RoundedCornerShape(percent = 29)),
-                decorationBox = {innerTextField ->
+                decorationBox = { innerTextField ->
                     Row(
                         Modifier
                             .background(BabyBlue, RoundedCornerShape(percent = 30))
                             .padding(0.dp)
                             .background(BabyBlue, RoundedCornerShape(percent = 29)),
                         verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceBetween )
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    )
                     {
                         Icon(Icons.Default.Search, tint = White, contentDescription = null)
-                        Box(modifier = Modifier
-                            .padding(top = 7.dp, bottom = 7.dp, end = 7.dp)
-                            .fillMaxWidth(0.9f)) {
-                            if(text.isEmpty()){
+                        Box(
+                            modifier = Modifier
+                                .padding(top = 7.dp, bottom = 7.dp, end = 7.dp)
+                                .fillMaxWidth(0.9f)
+                        ) {
+                            if (text.isEmpty()) {
                                 Text(
-                                    text="请输入搜索的URL",
+                                    text = "请输入搜索的URL",
                                     style = TextStyle(
-                                        color=Color(107, 118, 179)
+                                        color = Color(107, 118, 179)
                                     )
                                 )
                             }
                             innerTextField()
                         }
-                        if(text.isNotEmpty()){
+                        if (text.isNotEmpty()) {
                             Icon(Icons.Default.Close, tint = Color(107, 118, 179),
                                 contentDescription = null,
-                                modifier = Modifier.clickable { text="" }
+                                modifier = Modifier.clickable { text = "" }
                             )
-                        }else{
+                        } else {
                             Spacer(modifier = Modifier.width(10.dp))
                         }
                     }
@@ -183,30 +225,32 @@ fun SearchText(){
                 dialogState = openNormAlertDialog,
                 msg=msg.value
             )*/
-            Button(onClick = {
-                isURL= UrlJudgeUtil().getCompleteUrl(text)
-                if (isURL){
-                val call: Call<ResponseBody> = RequestUtil.service.getQRCode(text)
-                call.enqueue(
-                    object : Callback<ResponseBody> {
-                        override fun onResponse(
-                            call: Call<ResponseBody>,
-                            response: Response<ResponseBody>
-                        ) {
-                            val s: String = response.body()!!.string()
+            Button(
+                onClick = {
+                    isURL = UrlJudgeUtil().getCompleteUrl(text)
+                    if (isURL) {
+                        val call: Call<ResponseBody> = RequestUtil.service.getQRCode(text)
+                        call.enqueue(
+                            object : Callback<ResponseBody> {
+                                override fun onResponse(
+                                    call: Call<ResponseBody>,
+                                    response: Response<ResponseBody>
+                                ) {
+                                    val s: String = response.body()!!.string()
 //                            msg.value=s
 //                            openNormAlertDialog.value = !openNormAlertDialog.value
-                            resultsText = s
-                            focusManager.clearFocus()
-                        }
+                                    resultsText = s
+                                    focusManager.clearFocus()
+                                }
 
-                        override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                            LogUtil.logInfo(t.toString())
+                                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                                    LogUtil.logInfo(t.toString())
 
-                        }
+                                }
+                            }
+                        )
                     }
-                )
-            } },
+                },
                 shape = RoundedCornerShape(20.dp),
                 modifier = Modifier
                     .padding(8.dp, 0.dp)
@@ -219,12 +263,12 @@ fun SearchText(){
             ) {
                 Text(text = "Check!")
             }
-            if(!isURL){
+            if (!isURL) {
                 Box(
                     contentAlignment = Alignment.Center,
                     modifier = Modifier.fillMaxWidth()
-                ){
-                    Text(text="请输入有效的URL!", color = YuRed, fontWeight = FontWeight.W700)
+                ) {
+                    Text(text = "请输入有效的URL!", color = YuRed, fontWeight = FontWeight.W700)
                 }
             }
             LabelCard(resultsText)
@@ -237,7 +281,7 @@ fun SearchText(){
 @Composable
 fun AlertDialogComponent(
     dialogState: MutableState<Boolean>,
-    msg:String
+    msg: String
 ) {
     if (dialogState.value) {
         AlertDialog(
@@ -268,22 +312,25 @@ fun AlertDialogComponent(
 
 @Composable
 fun LabelCard(
-    resultsText:String
-){
+    resultsText: String
+) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(10.dp, 15.dp),
         contentAlignment = Alignment.Center
-    ){
-        Card(shape = RoundedCornerShape(20.dp),
+    ) {
+        Card(
+            shape = RoundedCornerShape(20.dp),
             elevation = 10.dp,
             modifier = Modifier
                 .fillMaxWidth()
         ) {
-            Box(modifier = Modifier
-                .fillMaxWidth()
-                .background(CardBlue)){
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(CardBlue)
+            ) {
                 Text(text = resultsText, color = White, modifier = Modifier.padding(15.dp))
             }
         }
